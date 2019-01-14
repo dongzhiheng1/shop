e318a1fb72d24d00dc9d9a416761ab7bf188c5d0
dongzhiheng <1428511549@qq.com>
2019-01-14 09:57:28 +0800

    修改

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -1,17 +1,17 @@
 @extends('layout.bst')
 @section('content')
-<form class="form-horizontal" method="post" action="/user/login" style="margin-top: 50px">
+<form class="form-horizontal" method="post" action="/user/login">
     {{csrf_field()}}
     <div class="form-group">
         <label for="inputEmail3" class="col-sm-2 control-label">账号</label>
         <div class="col-sm-10">
-            <input type="text" class="form-control" name="u_name" style="width:200px;" >
+            <input type="text" class="form-control" name="u_name" >
         </div>
     </div>
     <div class="form-group">
         <label for="inputPassword3" class="col-sm-2 control-label">密码</label>
         <div class="col-sm-10">
-            <input type="password" class="form-control" name="u_pwd" style="width:200px;">
+            <input type="password" class="form-control" name="u_pwd">
         </div>
     </div>
     <div class="form-group">
--- resources/views/user/reg.blade.php
+++ resources/views/user/reg.blade.php
@@ -1,35 +1,35 @@
 @extends('layout.bst')
 @section('content')
-<form class="form-horizontal" method="post" action="/user/reg" style="margin-top: 50">
+<form class="form-horizontal" method="post" action="/user/reg">
     {{csrf_field()}}
     <div class="form-group">
         <label for="inputEmail3" class="col-sm-2 control-label">账号</label>
         <div class="col-sm-10">
-            <input type="text" class="form-control" name="u_name" style="width:200px;">
+            <input type="text" class="form-control" name="u_name">
         </div>
     </div>
     <div class="form-group">
         <label for="inputPassword3" class="col-sm-2 control-label">密码</label>
         <div class="col-sm-10">
-            <input type="password" class="form-control" name="u_pwd" style="width:200px;">
+            <input type="password" class="form-control" name="u_pwd">
         </div>
     </div>
     <div class="form-group">
         <label for="inputPassword3" class="col-sm-2 control-label">确认密码</label>
         <div class="col-sm-10">
-            <input type="password" class="form-control" name="u_pwd2" style="width:200px;">
+            <input type="password" class="form-control" name="u_pwd2">
         </div>
     </div>
     <div class="form-group">
         <label for="inputEmail3" class="col-sm-2 control-label">年龄</label>
         <div class="col-sm-10">
-            <input type="text" class="form-control" name="u_age" style="width:200px;">
+            <input type="text" class="form-control" name="u_age">
         </div>
     </div>
     <div class="form-group">
         <label for="inputEmail3" class="col-sm-2 control-label">邮箱</label>
         <div class="col-sm-10">
-            <input type="text" class="form-control" name="u_email" style="width:200px;">
+            <input type="text" class="form-control" name="u_email">
         </div>
     </div>
     <div class="form-group">
