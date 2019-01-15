87a782731a8f86e8cbda77cffecbf2a933d188ea
dongzhiheng <1428511549@qq.com>
2019-01-15 11:15:25 +0800

    同步修改数据库

--- app/Http/Controllers/Pay/AlipayController.php
+++ app/Http/Controllers/Pay/AlipayController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Pay;
 
+use App\Model\OrderModel;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use GuzzleHttp\Client;
@@ -17,7 +18,6 @@ class AlipayController extends Controller
 
     public function __construct()
     {
-
         $this->app_id=env('ALIPAY_APPID');
         $this->gate_way=env('ALIPAY_GATEWAY');
         $this->notify_url=env('ALIPAY_NOTIFY');
@@ -112,6 +112,7 @@ class AlipayController extends Controller
     public function aliReturn()
     {
         echo '<pre>';print_r($_GET);echo '</pre>';
+        echo "支付成功";
         //验签 支付宝的公钥
         if(!$this->verify()){
             echo 'error';
@@ -125,19 +126,54 @@ class AlipayController extends Controller
         $log_str= '>>>>'.date('Y-m-d H:i:s').$data."<<<<\n\n";
         //记录日志
         file_put_contents('logs/alipay.log',$log_str,FILE_APPEND);
-        $res=$this->verify();
+        $res=$this->verify($_POST);
         if($res===false){
-            echo "error";
             //记录日志 验签失败
+            $log_str.="Sign Failed!<<<< \n\n";
+            file_put_contents('logs/alipay.log',$log_str,FILE_APPEND);
+        }else{
+            $log_str.="Sign OK<<<<\n\n";
+            file_put_contents('logs/alipay.log',$log_str,FILE_APPEND);
         }
-
+        //验证订单状态
+        if($_POST['trade_status']=='TRADE_SUCCESS'){
+            //更新订单状态
+            $oid=$_POST['out_trade_no'];
+            $info=[
+                'is_pay'=>1,//支付状态 0未支付 1已支付
+                'pay_amount'=>$_POST['total_amount']*100,//支付金额
+                'pay_time'=>strtotime($_POST['gmt_payment']),//支付时间
+                'plat_oid'=>$_POST['trade_no'], //支付宝订单号
+                'plat'=>1, //平台编号 1支付宝 2微信
+            ];
+            OrderModel::where(['oid'=>$oid])->update($info);
+        }
         //处理订单逻辑
         $this->dealOrder($_POST);
         echo "success";
     }
     //验签
-    function verify(){
-        return true;
+    function verify($params){
+        $sign = $params['sign'];
+        $params['sign_type'] = null;
+        $params['sign'] = null;
+
+        //读取公钥文件
+        $pubKey = file_get_contents($this->aliPubKey);
+        $pubKey = "-----BEGIN PUBLIC KEY-----\n" .
+            wordwrap($pubKey, 64, "\n", true) .
+            "\n-----END PUBLIC KEY-----";
+        //转换为openssl格式密钥
+
+        $res = openssl_get_publickey($pubKey);
+        ($res) or die('支付宝RSA公钥错误。请检查公钥文件格式是否正确');
+
+        //调用openssl内置方法验签，返回bool值
+
+        $result = (openssl_verify($this->getSignContent($params), base64_decode($sign), $res, OPENSSL_ALGO_SHA256)===1);
+        openssl_free_key($res);
+
+        return $result;
     }
 
     //处理订单逻辑 更新订单 支付状态 更新订单支付金额 支付时间
