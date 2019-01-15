9717c8cf627eaba1d18b843e2cfbce1be467bc86
dongzhiheng <1428511549@qq.com>
2019-01-15 18:49:11 +0800

    提交

--- app/Model/OrderModel.php
+++ app/Model/OrderModel.php
@@ -10,6 +10,6 @@ class OrderModel extends Model
     public $timestamps = false;
     //生成订单号
     public static  function generateOrderSN(){
-        return date('ymd').rand(111,999).rand(222,999);
+        return date('YmdHi').rand(11111,99999).rand(2222,9999);
     }
 }
