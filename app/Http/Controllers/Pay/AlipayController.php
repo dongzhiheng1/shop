716728f8b265a12747384ac6a0c0857c43b02ed6
dongzhiheng <1428511549@qq.com>
2019-01-15 13:43:53 +0800

    异步修改

--- app/Http/Controllers/Pay/AlipayController.php
+++ app/Http/Controllers/Pay/AlipayController.php
@@ -28,6 +28,12 @@ class AlipayController extends Controller
     //请求订单服务 处理订单逻辑
     public function pay($order_id){
         $OrderInfo=OrderModel::where(['order_id'=>$order_id])->first()->toArray();
+        if($OrderInfo['is_pay']==1){
+            die('订单已支付,请勿重复支付');
+        }
+        if($OrderInfo['is_delete']==1){
+            die('订单已删除,无法正常支付');
+        }
         $bizcont=[
             'subject'=>'goods'.$order_id,
             'out_trade_no'=>$order_id,
