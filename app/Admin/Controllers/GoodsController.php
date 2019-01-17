915342485b95a92e9aa0a704b1411637d01a40cb
dongzhiheng <1428511549@qq.com>
2019-01-17 18:27:30 +0800

    富文本编辑器

--- app/Admin/Controllers/GoodsController.php
+++ app/Admin/Controllers/GoodsController.php
@@ -2,14 +2,13 @@
 
 namespace App\Admin\Controllers;
 
+use App\model\GoodsModel;
 use App\Http\Controllers\Controller;
 use Encore\Admin\Controllers\HasResourceActions;
-use Encore\Admin\Layout\Content;
-use Encore\Admin\Grid;
-use Encore\Admin\Show;
 use Encore\Admin\Form;
-
-use App\Model\GoodsModel;
+use Encore\Admin\Grid;
+use Encore\Admin\Layout\Content;
+use Encore\Admin\Show;
 
 class GoodsController extends Controller
 {
@@ -24,31 +23,38 @@ class GoodsController extends Controller
     public function index(Content $content)
     {
         return $content
-            ->header('商品管理')
-            ->description('商品列表')
+            ->header('Index')
+            ->description('description')
             ->body($this->grid());
     }
 
     /**
      * Show interface.
      *
-     * @param mixed   $id
+     * @param mixed $id
      * @param Content $content
      * @return Content
      */
+    public function show($id, Content $content)
+    {
+        return $content
+            ->header('Detail')
+            ->description('description')
+            ->body($this->detail($id));
+    }
 
     /**
      * Edit interface.
      *
-     * @param mixed   $id
+     * @param mixed $id
      * @param Content $content
      * @return Content
      */
     public function edit($id, Content $content)
     {
         return $content
-            ->header('商品管理')
-            ->description('编辑')
+            ->header('Edit')
+            ->description('description')
             ->body($this->form()->edit($id));
     }
 
@@ -61,8 +67,8 @@ class GoodsController extends Controller
     public function create(Content $content)
     {
         return $content
-            ->header('商品管理')
-            ->description('添加')
+            ->header('Create')
+            ->description('description')
             ->body($this->form());
     }
 
@@ -73,44 +79,37 @@ class GoodsController extends Controller
      */
     protected function grid()
     {
-        $grid = new Grid(new GoodsModel());
-        $grid->model()->orderBy('goods_id','desc');
-        $grid->goods_id('商品ID');
-        $grid->goods_name('商品名称');
-        $grid->goods_num('库存');
-        $grid->goods_price('商品价格');
-        $grid->created_at('添加时间');
-//        (function($time){
-//            return date('Y-m-d H:i:s',$time);
-//        });
+        $grid = new Grid(new GoodsModel);
+
+        $grid->goods_id('Goods id');
+        $grid->goods_name('Goods name');
+        $grid->goods_num('Goods num');
+        $grid->goods_price('Goods price');
+        $grid->created_at('Created at');
+        $grid->updated_at('Updated at');
+
         return $grid;
     }
-    public function show($id, Content $content)
-    {
-        return $content
-            ->header('查看商品')
-            ->description('查看')
-            ->body($this->detail($id));
-    }
 
+    /**
+     * Make a show builder.
+     *
+     * @param mixed $id
+     * @return Show
+     */
     protected function detail($id)
     {
         $show = new Show(GoodsModel::findOrFail($id));
 
-        $show->goods_id('goods_id');
-        $show->goods_name('goods_name');
-        $show->goods_num('goods_num');
-        $show->goods_price('商品价格');
-        $show->created_at('添加时间');
+        $show->goods_id('Goods id');
+        $show->goods_name('Goods name');
+        $show->goods_num('Goods num');
+        $show->goods_price('Goods price');
+        $show->created_at('Created at');
+        $show->updated_at('Updated at');
 
         return $show;
     }
-    /**
-     * Make a show builder.
-     *
-     * @param mixed   $id
-     * @return Show
-     */
 
     /**
      * Make a form builder.
@@ -119,14 +118,13 @@ class GoodsController extends Controller
      */
     protected function form()
     {
-        $form = new Form(new GoodsModel());
+        $form = new Form(new GoodsModel);
 
-        $form->display('goods_id', '商品ID');
-        $form->text('goods_name', '商品名称');
-        $form->number('goods_num', '库存');
-        $form->currency('goods_price', '价格')->symbol('¥');
+        $form->text('goods_name', 'Goods name');
+        $form->number('goods_num', 'Goods num');
+        $form->number('goods_price', 'Goods price');
+        $form->ckeditor('content');
 
         return $form;
-
     }
 }
--- app/Admin/bootstrap.php
+++ app/Admin/bootstrap.php
@@ -1,5 +1,8 @@
 <?php
+use App\Admin\Extensions\Form\CKEditor;
+use Encore\Admin\Form;
 
+Form::extend('ckeditor', CKEditor::class);
 /**
  * Laravel-admin - admin builder based on Laravel.
  * @author z-song <https://github.com/z-song>
@@ -19,3 +22,4 @@
  */
 
 Encore\Admin\Form::forget(['map', 'editor']);
+
