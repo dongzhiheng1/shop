82092f9209b806e128a8933e066f040865e4d585
dongzhiheng <1428511549@qq.com>
2019-01-17 15:39:07 +0800

    修改

--- app/Model/GoodsModel.php
+++ app/Model/GoodsModel.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class GoodsModel extends Model
 {
     public $table = 'p_goods';
-    public $timestamps =true;
+    //public $timestamps =true;
     public $primaryKey = 'goods_id';
 
     //获取某字段时 格式化 该字段的值
