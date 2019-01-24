bd25f8b62d5777c7d514ebcaed9c558c3568b342
dongzhiheng <1428511549@qq.com>
2019-01-24 10:36:48 +0800

    提交

--- resources/views/goods/index.blade.php
+++ resources/views/goods/index.blade.php
@@ -3,8 +3,8 @@
 @section('content')
 
 <div class="container">
-    <h1>{{$goods->goods_name}}</h1>
-    <span> 价格：￥{{$goods->goods_price/100}}</span>
+    <h1>{{$goods['goods_name']}}</h1>
+    <span> 价格：￥{{$goods['goods_price']/100}}</span>
     <form class="form-inline">
         <div class="form-group">
             <label class="sr-only" for="goods_num">Amount (in dollars)</label>
@@ -12,7 +12,7 @@
                 <input type="text" class="form-control" id="goods_num" value="1">
             </div>
         </div>
-        <input type="hidden" id="goods_id" value="{{$goods->goods_id}}">
+        <input type="hidden" id="goods_id" value="{{$goods['goods_id']}}">
         <button type="submit" class="btn btn-primary" id="add_cart_btn">加入购物车</button>
     </form>
 </div>
