121a25c85738ccab99609a2d52bd7cd5c2500abd
dongzhiheng <1428511549@qq.com>
2019-02-20 11:00:39 +0800

    语音

--- app/Http/Controllers/Weixin/WeixinController.php
+++ app/Http/Controllers/Weixin/WeixinController.php
@@ -66,6 +66,10 @@ class WeixinController extends Controller
              $this->dlVoice($xml->MediaId);
              $xml_response = '<xml><ToUserName><![CDATA['.$openid.']]></ToUserName><FromUserName><![CDATA['.$xml->ToUserName.']]></FromUserName><CreateTime>'.time().'</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA['. str_random(10) . ' >>> ' . date('Y-m-d H:i:s') .']]></Content></xml>';
              echo $xml_response;
+         }elseif($xml->MsgType=='video') {        //处理语音信息
+             $this->dlVideo($xml->MediaId);
+             $xml_response = '<xml><ToUserName><![CDATA['.$openid.']]></ToUserName><FromUserName><![CDATA['.$xml->ToUserName.']]></FromUserName><CreateTime>'.time().'</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA['. str_random(10) . ' >>> ' . date('Y-m-d H:i:s') .']]></Content></xml>';
+             echo $xml_response;
          }
      }
         if ($event == 'subscribe') {
@@ -204,6 +208,27 @@ class WeixinController extends Controller
 
         }else{      //保存失败
 
+        }
+    }
+    public function dlVideo($media_id)
+    {
+        $url = 'https://api.weixin.qq.com/cgi-bin/media/get?access_token='.$this->getWXAccessToken().'&media_id='.$media_id;
+
+        $client = new GuzzleHttp\Client();
+        $response = $client->get($url);
+        //$h = $response->getHeaders();
+        //echo '<pre>';print_r($h);echo '</pre>';die;
+        //获取文件名
+        $file_info = $response->getHeader('Content-disposition');
+        $file_name = substr(rtrim($file_info[0],'"'),-20);
+
+        $wx_image_path = 'wx/video/'.$file_name;
+        //保存图片
+        $r = Storage::disk('local')->put($wx_image_path,$response->getBody());
+        if($r){     //保存成功
+           echo "保存成功";
+        }else{      //保存失败
+           echo "保存失败";
         }
     }
     public function wxMenu()
