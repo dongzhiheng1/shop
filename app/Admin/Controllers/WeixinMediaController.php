f9612861c1bd74e6367ed487dc5cef7cbbc26eea
dongzhiheng1 <1428511549@qq.com>
2019-02-20 19:45:45 +0800

    修改

--- app/Admin/Controllers/WeixinMediaController.php
+++ app/Admin/Controllers/WeixinMediaController.php
@@ -90,7 +90,9 @@ class WeixinMediaController extends Controller
         $grid->media_id('Media id');
         $grid->format('Format');
         $grid->msg_id('Msg id');
-        $grid->local_file_name('Local file name');
+        $grid->local_file_name('Local file name')->display(function($image){
+            return "<img src='https://dzh.wangby.cn/wx/image/".$image."'>";
+        });;
         $grid->local_file_path('Local file path');
 
         return $grid;
