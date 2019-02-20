4927d328e24a80d5f377f725bbd0de5d7bcb6f05
dongzhiheng <1428511549@qq.com>
2019-02-20 09:24:12 +0800

    微信修改

--- app/Http/Controllers/Weixin/WeixinController.php
+++ app/Http/Controllers/Weixin/WeixinController.php
@@ -64,7 +64,7 @@ class WeixinController extends Controller
              }
 
          }
-         exit();
+         //exit();
      }
         if ($event == 'subscribe') {
                            //用户openid
