f79cd09f29d033b9ed082e83c3a294d343d5c1b2
dongzhiheng1 <1428511549@qq.com>
2019-02-21 10:57:41 +0800

    日志

--- app/Http/Controllers/Weixin/WeixinController.php
+++ app/Http/Controllers/Weixin/WeixinController.php
@@ -43,6 +43,9 @@ class WeixinController extends Controller
     {
         $data = file_get_contents("php://input");
         $xml = simplexml_load_string($data);
+        //记录日志
+        $log_str = date('Y-m-d H:i:s') . "\n" . $data . "\n<<<<<<<";
+        file_put_contents('logs/wx_event.log',$log_str,FILE_APPEND);
 
         //解析XML
              //将 xml字符串 转换成对象
