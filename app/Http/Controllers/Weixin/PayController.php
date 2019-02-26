8fa9369dfce6498e832060efb32fe0d85df96119
dongzhiheng1 <1428511549@qq.com>
2019-02-26 15:51:33 +0800

    微信支付

--- app/Http/Controllers/Weixin/PayController.php
+++ app/Http/Controllers/Weixin/PayController.php
@@ -12,30 +12,40 @@ class PayController extends Controller
     public $weixin_unifiedorder_url = 'https://api.mch.weixin.qq.com/pay/unifiedorder';
     //通知回调
      public $weixin_notify_url='https://dzh.wangby.cn/weixin/pay/notice';
-     public function test(){
-         $total_fee=1; //用户支付的总金额
-         $order_id=OrderModel::generateOrderSN();
-         $orderInfo=[
-             'appid'=>env('WEIXIN_APPID_0'),
-             'mch_id'=>env('WEIXIN_MCH_ID'),
-             'nonce_str'=>str_random(16),
-             'sign_type'=>'MD5',
-             'body'=>'测试订单-'.mt_rand(1111,9999),
-             'out_trade_no'=> $order_id,
-             'total_free'=>$total_fee,
-             'spbill_create_ip'=>$_SERVER['REMOTE_ADDR'],
-             'notify'=>$this->weixin_notify_url,
-             'trade_type'=>'NATIVE'
-         ];
+    public function test()
+    {
 
-         $this->values=[];
-         $this->values=$orderInfo;
-         $this->SetSign();
-         $xml=$this->ToXml();
-         $rs = $this->postXmlCurl($xml, $this->weixin_unifiedorder_url, $useCert = false, $second = 30);
-         $data =simplexml_load_string($rs);
-         echo 'code_url: '.$data->code_url;echo '<br>';
-     }
+
+        //
+        $total_fee = 1;         //用户要支付的总金额
+        $order_id = OrderModel::generateOrderSN();
+
+        $order_info = [
+            'appid'         =>  env('WEIXIN_APPID_0'),      //微信支付绑定的服务号的APPID
+            'mch_id'        =>  env('WEIXIN_MCH_ID'),       // 商户ID
+            'nonce_str'     => str_random(16),             // 随机字符串
+            'sign_type'     => 'MD5',
+            'body'          => '测试订单-'.mt_rand(1111,9999) . str_random(6),
+            'out_trade_no'  => $order_id,                       //本地订单号
+            'total_fee'     => $total_fee,
+            'spbill_create_ip'  => $_SERVER['REMOTE_ADDR'],     //客户端IP
+            'notify_url'    => $this->weixin_notify_url,        //通知回调地址
+            'trade_type'    => 'NATIVE'                         // 交易类型
+        ];
+
+
+        $this->values = [];
+        $this->values = $order_info;
+        $this->SetSign();
+
+        $xml = $this->ToXml();      //将数组转换为XML
+        $rs = $this->postXmlCurl($xml, $this->weixin_unifiedorder_url, $useCert = false, $second = 30);
+        $data =  simplexml_load_string($rs);
+        echo 'code_url: '.$data->code_url;echo '<br>';
+//        die;
+        //echo '<pre>';print_r($data);echo '</pre>';
+        //将 code_url 返回给前端，前端生成 支付二维码
+    }
     public function ToXml(){
         if(!is_array($this->values) || count($this->values)<=0){
             die("数组数据异常!");
