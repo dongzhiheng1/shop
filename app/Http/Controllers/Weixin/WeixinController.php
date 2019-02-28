f10a71fcfc83c4d1f75bde14f9d9e299f31ed3be
dongzhiheng1 <1428511549@qq.com>
2019-02-28 18:22:12 +0800

    登录2

--- app/Http/Controllers/Weixin/WeixinController.php
+++ app/Http/Controllers/Weixin/WeixinController.php
@@ -472,34 +472,67 @@ class WeixinController extends Controller
         }
         return $arr;
     }
-    public function getcode(){
-        // 1 回调拿到 code (用户确认登录后 微信会跳 redirect )
-        echo '<pre>';print_r($_GET);echo '</pre>';echo '<hr>';
-        echo '<pre>';print_r($_POST);echo '</pre>';
-
-        $code = $_GET['code'];          // code
-
-        //2 用code换取access_token 请求接口
-
-        $token_url = 'https://api.weixin.qq.com/sns/oauth2/access_token?appid=wxe24f70961302b5a5&secret=0f121743ff20a3a454e4a12aeecef4be&code='.$code.'&grant_type=authorization_code';
-        $token_json = file_get_contents($token_url);
-        $token_arr = json_decode($token_json,true);
-        echo '<hr>';
-        echo '<pre>';print_r($token_arr);echo '</pre>';
-
-        $access_token = $token_arr['access_token'];
-        $openid = $token_arr['openid'];
-
-        // 3 携带token  获取用户信息
-        $user_info_url = 'https://api.weixin.qq.com/sns/userinfo?access_token='.$access_token.'&openid='.$openid.'&lang=zh_CN';
-        $user_json = file_get_contents($user_info_url);
-
-        $user_arr = json_decode($user_json,true);
-        echo '<hr>';
-        echo '<pre>';print_r($user_arr);echo '</pre>';
-
+    public function getcode(Request $request){
+            //print_r($_GET);
+            $code = $request->input('code');
+            //code换去access——token请求接口
+            $token_url = 'https://api.weixin.qq.com/sns/oauth2/access_token?appid=wxe24f70961302b5a5&secret=0f121743ff20a3a454e4a12aeecef4be&code='.$code.'&grant_type=authorization_code';
+            $token_json = file_get_contents($token_url);
+            $token_arr = json_decode($token_json,true);
+            //echo '<hr>';
+            //echo '<pre>';print_r($token_arr);echo '</pre>';
+            $access_token = $token_arr['access_token'];
+            $openid = $token_arr['openid'];
+            // 3 携带token  获取用户信息
+            $user_info_url = 'https://api.weixin.qq.com/sns/userinfo?access_token='.$access_token.'&openid='.$openid.'&lang=zh_CN';
+            $user_json = file_get_contents($user_info_url);
+            $user_arr = json_decode($user_json,true);
+            //echo '<hr>';
+            //echo '<pre>';print_r($user_arr);echo '</pre>';
+            //查询数据库中是否存在该账号
+            $unionid = $user_arr['unionid'];
+            $where = [
+                'union_id'   =>  $unionid
+            ];
+            $wx_user_info = WxUserModel::where($where)->first();
+            if($wx_user_info){
+                $user_info = UserModel::where(['wechat_id'=>$wx_user_info->id])->first();
+            }
+            if(empty($wx_user_info)){
+                //第一次登录
+                $data = [
+                    'openid'        =>  $user_arr['openid'],
+                    'nickname'      =>  $user_arr['nickname'],
+                    'sex'           =>  $user_arr['sex'],
+                    'headimgurl'    =>  $user_arr['headimgurl'],
+                    'unionid'      =>  $unionid,
+                    'add_time'      =>  time()
+                ];
+                $wechat_id = WeixinUser::insertGetId($data);
+                $rs = UserModel::insertGetId(['wechat_id'=>$wechat_id]);
+                if($rs){
+                    $token=substr(md5(time().mt_rand(1,99999)),10,10);
+                    setcookie('uid',$rs,time()+86400,'/','shop.com',false,true);
+                    setcookie('token',$token,time()+86400,'/user','',false,true);
+                    $request->session()->put('u_token',$token);
+                    $request->session()->put('uid',$rs);
+                    echo '注册成功';
+                    header("refresh:2,url='/user/center'");
+                }else{
+                    echo '注册失败';
+                }
+                exit;
+            }
+            $token=substr(md5(time().mt_rand(1,99999)),10,10);
+            setcookie('uid',$user_info->uid,time()+86400,'/','shop.com',false,true);
+            setcookie('token',$token,time()+86400,'/user','',false,true);
+            $request->session()->put('u_token',$token);
+            $request->session()->put('uid',$user_info->uid);
+            header("refresh:2,url='/user/center'");
     }
     public function wxlogin(){
         return view('weixin/wxlogin');
     }
+
+
 }
